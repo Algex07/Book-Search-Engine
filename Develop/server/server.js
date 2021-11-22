7e3dbdd779dc6179023d3cdc96fe90acb0ba0685
Amara <amar_algu@hotmail.com>
2021-11-21 22:25:36 -0500

    created Apollo server componemts,schemas etc.

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,10 +2,22 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const {  ApolloServer } = require("apollo-server-express")
 
-const app = express();
+const { typeDefs, resolvers } = require("./schemas")
+const { authMiddleware } = require("./utils/auth")
+
+
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
+server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,5 +29,9 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, ()=>{
+   
+  console.log(`🌍 Api server running on port ${PORT}!`);
+  console.log(`Use GraphQL at http:localhost:${PORT}${server.graphqlPath}`);
+  });
 });
